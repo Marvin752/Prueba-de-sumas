dfc10eb5a34b3dd81bbc8a14914eb27658d8e0f4
Marvin Cámbara <marvin75205@gmail.com>
2024-02-12 13:45:37 -0600

    Prueba dos proyectos

---
+++ Divison/Program.cs
@@ -0,0 +1,8 @@
+﻿int no;
+int si;
+Console.Write("Ingrese un numero a dividir: ");
+no = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ingrese el segundo numero: ");
+si = Convert.ToInt32(Console.ReadLine());
+int resultc = no / si;
+Console.WriteLine("El resultado es: " +resultc);
