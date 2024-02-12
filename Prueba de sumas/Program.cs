c8c49d5d259629f80443d9c6cc35e4557ed95361
Alux <Alux@LAPTOP-A2BE218P>
2024-02-12 13:14:43 -0600

    Agregar archivos de proyecto.

---
+++ Prueba de sumas/Program.cs
@@ -0,0 +1,8 @@
+﻿int a;
+int b;
+Console.Write("Ingrese un numero: ");
+a= Convert.ToInt32(Console.ReadLine());
+Console.Write("Ingrese el segundo numero: ");
+b= Convert.ToInt32(Console.ReadLine());
+int result = a + b;
+Console.WriteLine("El resultado es: " + result);
